f1969d5b78c0c606cf1e0c8b688f6852e1f2c31a
maeval <maeval.nightingale@gmail.com>
2018-05-01 18:05:58 +0200

    Protect API with an integration with CUP (Cognito User Pool)

--- handler.js
+++ handler.js
@@ -2,7 +2,13 @@ const librariesManager = require('./lib/libraries-manager');
 
 
 exports.getLibraries = async (event) => {
+  // const { claims } = event.requestContext.authorizer;
   const result = await librariesManager.getLibraries();
+  /* const result = {
+    libraries,
+    username: claims['cognito:username'],
+    userId: claims.sub,
+  }; */
 
   const response = {
     statusCode: 200,
