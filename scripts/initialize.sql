4be98330dd0ef7ee43f9f684265cd982262135b1
Maev4l <>
2018-12-27 17:21:46 +0100

    Fuzzy search feature:  - do not use PG Full Text Search. So no need to have a dedicated FTS config

--- scripts/initialize.sql
+++ scripts/initialize.sql
@@ -1,6 +1,7 @@
 BEGIN;
 CREATE SCHEMA IF NOT EXISTS "celsus_core" AUTHORIZATION postgres;
 SET search_path TO celsus_core;
+CREATE EXTENSION IF NOT EXISTS unaccent;
 \i scripts/library.sql
 \i scripts/book.sql
 COMMIT;
