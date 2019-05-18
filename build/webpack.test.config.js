d1facb4e9f9b0336bacabeadcdbb457532467637
Maev4l <maeval.nightingale@gmail.com>
2019-05-18 14:44:26 +0200

    Attempt to enable code coverage

--- build/webpack.test.config.js
+++ build/webpack.test.config.js
@@ -1,8 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge');
-
+const path = require('path');
 const common = require('./webpack.base.config.js');
 
 module.exports = merge(common, {
   mode: 'development',
+  output: {
+    // use absolute paths in sourcemaps (important for debugging via IDE)
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]',
+  },
+  devtool: 'inline-cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)/,
+        include: path.resolve('lib'),
+        loader: 'istanbul-instrumenter-loader',
+        options: { esModules: true },
+      },
+    ],
+  },
 });
